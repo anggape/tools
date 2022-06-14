7d0bd8eab5fc2ba5747e7fd81e85d24af7e35bef
anggape <anggape.dev@gmail.com>
2022-06-14 11:44:41 +0700

    chore(prefs): set prefs

---
+++ prefs/Runtime/Prefs.cs
@@ -0,0 +1,21 @@
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace Ape
+{
+    public static class Prefs
+    {
+        public const string PrefsPrefix = "__ape_prefs:";
+
+        public static void Set(string key, object value)
+        {
+            var json = JsonConvert.SerializeObject(
+                value,
+                Formatting.None,
+                new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore }
+            );
+
+            PlayerPrefs.SetString($"{PrefsPrefix}{key}", json);
+        }
+    }
+}
